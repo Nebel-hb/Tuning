26b4fd108808bbf55371b140f723bf9eb47fcfa4
Nebel-hb <nebel.hb@gmail.com>
2021-06-20 01:29:43 +0000

    [add]validation rails-i18n

--- app/models/thank_you_comment.rb
+++ app/models/thank_you_comment.rb
@@ -1,5 +1,10 @@
 class ThankYouComment < ApplicationRecord
-  
+
    belongs_to :user, optional: true
    belongs_to :recruitement, optional: true
+
+   validates :user_id, presence: true
+   validates :recruitement_id, presence: true
+   validates :thanks_comment, presence: true
+
 end
