986d9359233d637592225d68fdcc8837953a1440
Nebel-hb <nebel.hb@gmail.com>
2021-06-27 15:00:55 +0000

    [fix]devise admins redirect

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -7,7 +7,7 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   # GET /resource/sign_up
   def new
   #   super
-  redirect_to new_admin_session_path
+    redirect_to new_admin_session_path
   end
 
   # POST /resource
