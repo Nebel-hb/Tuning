1a531e1c5c196d48be81b5f92f7000ead6302084
Nebel-hb <nebel.hb@gmail.com>
2021-07-08 12:57:12 +0000

    Suspension

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require turbolinks
 
 //= require moment
-//= require fullcalendar
+
 //= require jquery.jscroll.min.js
 
 //= require shards
@@ -25,7 +25,6 @@
 //= require_tree .
 
 /* global $*/
-
 
 
 //スクロールすると上部に固定させるための設定を関数でまとめる
@@ -137,63 +136,13 @@ let client_h = document.getElementsByClassName('.sel__box ').clientHeight;
 
 
 //カレンダー
-$(function () {
     // 画面遷移を検知
     $(document).on('turbolinks:load', function () {
-        if ($('#calendar').length) {
-
-            function Calendar() {
-                return $('#calendar').fullCalendar({
-                });
-            }
-            function clearCalendar() {
-                $('#calendar').html('');
-            }
-
-            $(document).on('turbolinks:load', function () {
-                Calendar();
-            });
-            $(document).on('turbolinks:before-cache', clearCalendar);
-
-            //events: '/events.json', 以下に追加
-            $('#calendar').fullCalendar({
-                events: '/events.json',
-                //カレンダー上部を年月で表示させる
-                titleFormat: 'YYYY年 M月',
-                //曜日を日本語表示
-                dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
-                //ボタンのレイアウト
-                header: {
-                    left: 'listWeek,dayGridMonth',
-                    center: 'title',
-                    right: ' today, prev,next'
-                },
-                //終了時刻がないイベントの表示間隔
-                defaultTimedEventDuration: '03:00:00',
-                buttonText: {
-                    prev: '<',
-                    next: '>',
-                    prevYear: '前年',
-                    nextYear: '翌年',
-                    today: '今日',
-                    month: '月',
-                    week: '週',
-                    day: '日'
-                },
-                // Drag & Drop & Resize
-                // editable: true,
-                //イベントの時間表示を２４時間に
-                timeFormat: "HH:mm",
-                //イベントの色を変える
-                eventColor: '#d0e39b',
-                //イベントの文字色を変える
-                eventTextColor: '#000000',
-                eventRender: function(event, element) {
-                    element.css("font-size", "0.5em");
-                    element.css("padding", "3px");
-                }
- 
-            });
-        }
-    });
+       document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
 });
