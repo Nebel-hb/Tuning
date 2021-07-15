878f352bef8ce879ac92351f7326fbba016158e3
Nebel-hb <nebel.hb@gmail.com>
2021-07-15 03:32:56 +0000

    [fix]chat scroll

--- app/assets/javascripts/public/rooms.js
+++ app/assets/javascripts/public/rooms.js
@@ -1,26 +1,3 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-
-// チャット　最下部にスクロール
-// document.addEventListener("turbolinks:load", () => {
-    // if(document.URL.match(/rooms\/+\d/)){
-    //  setTimeout(
-
-    // function scrollToEnd() {
-    //     ('#scroll').animate({ scrollEnd: $('#scroll')[0].scrollHeight});
-    //     var messageDetails = document.getElementById('scroll');
-    //     messageDetails.scrollTop = messageDetails.scrollHeight;
-    //     }, 1000);
-    // scrollToEnd()
-    // }
-    
-    // var element = document.getElementById('scroll'); // 移動させたい位置の要素を取得
-    // var rect = element.getBoundingClientRect();
-    // var position = rect.top;
-    // setTimeout( function() {
-    //     scrollTo(0, position);}
-    //     , 1000);
-// }
-// })
-
